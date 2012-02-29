383242402dece0a665f89fbbe65f6835227eae88
Josh Farneman <josh@mindvolt.com>
2012-02-29 10:51:35 -0600

    Tweaked prompt.

--- themes/farneman/farneman.theme.bash
+++ themes/farneman/farneman.theme.bash
@@ -2,8 +2,8 @@
 #farneman theme by Josh Farneman : farneman.net
 
 #git colors
-GIT_THEME_PROMPT_DIRTY=" ${bold_black}✗"
-GIT_THEME_PROMPT_CLEAN=" ${bold_white}✓"
+GIT_THEME_PROMPT_DIRTY=" ${white}!"
+GIT_THEME_PROMPT_CLEAN=""
 GIT_THEME_PROMPT_PREFIX=" ${green}["
 GIT_THEME_PROMPT_SUFFIX="${green}]"
 
@@ -12,7 +12,7 @@ SCM_NONE_CHAR="≠"
 
 function prompt_command() {
     #PS1="\n${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
-    PS1="\n${red}\u${yellow}:${bold_yellow}\w${yellow}: ${bold_white}$(scm_char)${green}$(scm_prompt_info) ${red}»${reset_color} "
+    PS1="\n${red}\u${yellow}[${bold_yellow}\w${yellow}]\n${bold_purple}$(scm_char)${green}$(scm_prompt_info) ${red}»${reset_color} "
 }
 
 PROMPT_COMMAND=prompt_command;
